164a13af768cb705729cd34aa05f91733d13a014
mohsin <k173634@nu.edu.pk>
2020-12-22 02:07:16 +0500

    Added Vote method in .sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -23,4 +23,19 @@ contract Election {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+
 }
