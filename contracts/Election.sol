d0326eb6dd0287f0a79e2dab85a9102db7d7e289
mohsin <k173634@nu.edu.pk>
2020-12-21 19:47:51 +0500

    building contract Election

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+    function Election () public {
+        candidate = "Candidate 1";
+    }
+}
