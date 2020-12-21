f37b8ead87be9d66149615a6c377b8474ea9814c
mohsin <k173634@nu.edu.pk>
2020-12-22 03:33:57 +0500

    Test to ensure double voting

--- test/election.js
+++ test/election.js
@@ -59,4 +59,28 @@ contract("Election", function(accounts) {
       assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
     });
   });
+
+  it("throws an exception for double voting", function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      candidateId = 2;
+      electionInstance.vote(candidateId, { from: accounts[1] });
+      return electionInstance.candidates(candidateId);
+    }).then(function(candidate) {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "accepts first vote");
+      // Try to vote again
+      return electionInstance.vote(candidateId, { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then(function(candidate1) {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return electionInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+    });
+  });
 });
