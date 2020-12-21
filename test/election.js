45a5daa6774f217baa91f508acd380255394bab4
mohsin <k173634@nu.edu.pk>
2020-12-22 03:32:58 +0500

    Vote count test added

--- test/election.js
+++ test/election.js
@@ -42,4 +42,21 @@ contract("Election", function(accounts) {
       assert.equal(voteCount, 1, "increments the candidate's vote count");
     })
   });
+
+  it("throws an exception for invalid candidates", function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.vote(99, { from: accounts[1] })
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then(function(candidate1) {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return electionInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+    });
+  });
 });
