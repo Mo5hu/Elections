5c03767341b0bf4105292a67d17c0370102f3e6a
mohsin <k173634@nu.edu.pk>
2020-12-22 02:06:25 +0500

    mapping for voters

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,6 +11,7 @@ contract Election {
     }
 
     mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
     uint public candidatesCount;
     // Constructor
     constructor () public {
@@ -22,4 +23,4 @@ contract Election {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
-}}
+}
