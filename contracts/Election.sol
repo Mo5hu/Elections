8e27aea8a3e2aa6ea53e7b1f827cbcaaf1a74097
mohsin <k173634@nu.edu.pk>
2020-12-21 19:49:57 +0500

    updated Version of compiler

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.5.16;
 
 contract Election {
     // Read/write candidate
     string public candidate;
 
     // Constructor
-    function Election () public {
+    constructor () public {
         candidate = "Candidate 1";
     }
 }
